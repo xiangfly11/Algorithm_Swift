89fd8923c0fd59a7452523bd4b1d55a335b04730
Jiaxiang <jason@thecover.co>
2021-03-17 22:52:02 +0800

    [fix]使用迭代算法判断回文链表。

--- PalindromeList/PalindromeList/LinkedList.swift
+++ PalindromeList/PalindromeList/LinkedList.swift
@@ -34,7 +34,7 @@ class LinkedList<T: Equatable> {
     }
     
     
-    func traverse(head: Node<T>?) {
+    func traversePrint(head: Node<T>?) {
         guard let h = head else {
             return
         }
@@ -42,12 +42,14 @@ class LinkedList<T: Equatable> {
         //前序遍历
         print(h.value)
         
-        traverse(head:h.next)
+        traversePrint(head:h.next)
         
         //后序遍历
 //        print(h.value)
     }
     
+    // MARK: 使用递归判断链表是否是回文链表
+    // 空间复杂度：O(n) 时间复杂度：O(n)
     var leftNode: Node<T>?
     func postTraverseListCompare(head: Node<T>?) -> Bool {
         guard let h = head else {
@@ -59,8 +61,56 @@ class LinkedList<T: Equatable> {
         return res
     }
     
-    func isPalindromList(head: Node<T>?) -> Bool {
+    func isPalindromListRecurisivly(head: Node<T>?) -> Bool {
         leftNode = head
         return postTraverseListCompare(head: head)
+    }
+    
+    // MARK: 使用迭代判断链表是否是回文
+    // 空间复杂度：O(1) 时间复杂度:O(n)
+    func reverse(head:Node<T>?) -> Node<T>? {
+        var pre: Node<T>?
+        var curr: Node<T>? = head
+        var next: Node<T>? = head
+        
+        while curr != nil {
+            next = curr?.next
+            curr?.next = pre
+            pre = curr
+            curr = next
+        }
+        
+        return pre
+    }
+    
+    func isPalindromListIteratively(head: Node<T>?) -> Bool {
+        var slow: Node<T>? = head
+        var fast: Node<T>? = head
+        
+        while fast != nil && fast?.next != nil {
+            slow = slow?.next
+            fast = fast?.next?.next
+        }
+        
+        //如果fast没有指向nil,那么链表个数为奇数，慢指针向前移动以为
+        if fast != nil {
+            slow = slow?.next
+        }
+        
+        //反转慢指针所指向节点之后的链表
+        var right = reverse(head: slow)
+        var left = head
+        //设置一个反转之后的右边的头节点，在最后用来还原这个链表
+        let originalRight = right
+        
+        while right != nil {
+            if left?.value != right?.value {
+                return false
+            }
+            
+            left = left?.next
+            right = right?.next
+        }
+        return true
     }
 }
--- PalindromeList/PalindromeList/main.swift
+++ PalindromeList/PalindromeList/main.swift
@@ -14,7 +14,7 @@ for i in 0 ... 5 {
     let _ = linkedList1.appendNode(node: node)
 }
 
-var i = 5
+var i = 3
 while i >= 0 {
     let node = ListNode<Int>.init()
     node.value = i
@@ -22,7 +22,8 @@ while i >= 0 {
     i = i - 1
 }
 
-linkedList1.traverse(head: linkedList1.head)
-let isPalindromList = linkedList1.isPalindromList(head: linkedList1.head)
+linkedList1.traversePrint(head: linkedList1.head)
+let isPalindromList1 = linkedList1.isPalindromListRecurisivly(head: linkedList1.head)
+let isPalindromList2 = linkedList1.isPalindromListIteratively(head: linkedList1.head)
 
 print("finished")
